d171c03906e2ce2d2f1cfd5c3af6f38a119515e4
AlejandraTaba <alejaydante2@gmail.com>
2025-05-17 16:04:12 -0500

    actualización trabajo

--- poo4.py
+++ poo4.py
@@ -1,25 +1,25 @@
-class Usuario:
+class user:
     def __init__(self, nombre):
         self.nombre = nombre
 
-class Libro:
+class book:
     def __init__(self, titulo):
         self.titulo = titulo
-        self.prestado_a = None  
+        self.prestado_a = []  
 
     def prestar(self, usuario):
-        if self.prestado_a is None:
+        if not self.prestado_a: 
             self.prestado_a = usuario
             return f"El libro '{self.titulo}' fue prestado a {usuario.nombre}."
         else:
             return f"El libro '{self.titulo}' ya está prestado a {self.prestado_a.nombre}."
 
     def devolver(self):
-        if self.prestado_a is None:
+        if not self.prestado_a:
             return f"El libro '{self.titulo}' no está prestado."
         else:
             usuario = self.prestado_a
-            self.prestado_a = None
+            self.prestado_a = []
             return f"{usuario.nombre} devolvió el libro '{self.titulo}'."
 
     def estado(self):
@@ -28,11 +28,11 @@ class Libro:
         else:
             return "Disponible"
 
-usuario1 = Usuario("Alex")
-libro1 = Libro("Los secretos de la mente millonaria")
+user1 = user("Alex")
+book1 = book("Los secretos de la mente millonaria")
 
-print(libro1.estado())
-print(libro1.prestar(usuario1))
-print(libro1.estado())
-print(libro1.devolver())
-print(libro1.estado())
+print(book1.estado())
+print(book1.prestar(user1))
+print(book1.estado())
+print(book1.devolver())
+print(book1.estado())
